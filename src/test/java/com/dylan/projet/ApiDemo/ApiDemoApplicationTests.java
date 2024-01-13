0419cee2c41607413c6340f9c2f8bc0f75a66624
Dylan T <dylantizguine@gmail.com>
2024-01-13 19:29:54 +0100

    first commit

---
+++ src/test/java/com/dylan/projet/ApiDemo/ApiDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dylan.projet.ApiDemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDemoApplicationTests {
+/**
+	@Test
+	void contextLoads() {
+	}*/
+
+}
